469f802f4ccf9090851de3e980510cb48a143c2e
Ana Viktoriv <anaviktoriv@gmail.com>
2020-08-12 07:23:44 +0200

    ajout de deux tables User et Employé + ajout de mapping enum:string + verif de valeurs pour enum

--- SmartLiving/src/Controller/TestController.php
+++ SmartLiving/src/Controller/TestController.php
@@ -4,20 +4,40 @@
 namespace App\Controller;
 
 
+use App\Entity\Employee;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 
 
 //Test controller
-class TestController {
+class TestController extends AbstractController {
 
     /**
-     * @Route("/")
+     * @Route("/", name="app_test")
      */
     public function index() {
 
-        return new Response('Test');
+        return $this->render('test.html.twig');
+    }
+
+    /**
+     * @Route("/employee/add", name="employee_add")
+     */
+    public function addNewEmployee(EntityManagerInterface $em) {
+        $employee = new Employee();
+        $employee->setFirstName('Jane')
+            ->setLastName('Doe')
+            ->setPhone('+55555555')
+            ->setAddress('123 Main St')
+            ->setStatus('bla');
+
+        $em->persist($employee);
+        $em->flush();
+
+        return new Response("Success");
     }
 
 
---
+++ SmartLiving/src/Entity/Employee.php
@@ -0,0 +1,117 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\EmployeeRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=EmployeeRepository::class)
+ */
+class Employee
+{
+    const STOCK_MANAGER = 'stock_manager';
+    const SALESPERSON = 'salesperson';
+    const BUSINESS_SALESPERSON = 'business_salesperson';
+
+
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=50)
+     */
+    private $firstName;
+
+    /**
+     * @ORM\Column(type="string", length=50)
+     */
+    private $lastName;
+
+    /**
+     * @ORM\Column(type="string", length=15, nullable=true)
+     */
+    private $phone;
+
+    /**
+     * @ORM\Column(type="string", length=100, nullable=true)
+     */
+    private $address;
+
+    /**
+     * @ORM\Column(type="string", columnDefinition="enum('stock_manager', 'salesperson', 'business_salesperson' )")
+     */
+    private $status;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getFirstName(): ?string
+    {
+        return $this->firstName;
+    }
+
+    public function setFirstName(string $firstName): self
+    {
+        $this->firstName = $firstName;
+
+        return $this;
+    }
+
+    public function getLastName(): ?string
+    {
+        return $this->lastName;
+    }
+
+    public function setLastName(string $lastName): self
+    {
+        $this->lastName = $lastName;
+
+        return $this;
+    }
+
+    public function getPhone(): ?string
+    {
+        return $this->phone;
+    }
+
+    public function setPhone(?string $phone): self
+    {
+        $this->phone = $phone;
+
+        return $this;
+    }
+
+    public function getAddress(): ?string
+    {
+        return $this->address;
+    }
+
+    public function setAddress(?string $address): self
+    {
+        $this->address = $address;
+
+        return $this;
+    }
+
+    public function getStatus(): ?string
+    {
+        return $this->status;
+    }
+
+    public function setStatus(string $status): self
+    {
+        if (!in_array($status, array(self::BUSINESS_SALESPERSON, self::SALESPERSON, self::STOCK_MANAGER))){
+            throw new \InvalidArgumentException('Invalid employee status.');
+        }
+        $this->status = $status;
+
+        return $this;
+    }
+}
