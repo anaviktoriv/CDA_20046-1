57dd5479d5533b18c3a3b86178839c05cc3c5289
Ana Viktoriv <anaviktoriv@gmail.com>
2020-08-18 10:00:39 +0200

    ajout de dosscier images css + navbar twig

---
+++ SmartLiving/src/Twig/AppExtension.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Twig;
+
+use Detection\MobileDetect;
+use Twig\Extension\AbstractExtension;
+//use Twig\TwigFilter;
+use Twig\TwigFunction;
+
+class AppExtension extends AbstractExtension
+{
+/*    public function getFilters(): array
+    {
+        return [
+            // If your filter generates SAFE HTML, you should add a third
+            // parameter: ['is_safe' => ['html']]
+            // Reference: https://twig.symfony.com/doc/2.x/advanced.html#automatic-escaping
+            new TwigFilter('filter_name', [$this, 'doSomething']),
+        ];
+    }*/
+
+    public function getFunctions(): array
+    {
+        return [
+            new TwigFunction('is_mobile', [$this, 'checkIfMobile']),
+        ];
+    }
+
+    public function checkIfMobile()
+    {
+        $detect = new MobileDetect();
+ dump($detect);
+        if ( $detect->isMobile() ) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+
+
+}
