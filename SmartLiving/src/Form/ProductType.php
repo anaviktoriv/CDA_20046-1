dd78ac8843f20c04fe199c6134520bc6717561d0
Gaulthier Guérin <gaulthier.guerin@gmail.com>
2020-08-18 15:21:26 +0200

    carousel produits tendances OK - section promo WIP

---
+++ SmartLiving/src/Form/ProductType.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Product;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ProductType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('title')
+            ->add('description')
+            ->add('status')
+            ->add('photo')
+            ->add('unitPrice')
+            ->add('stock')
+            ->add('restockDate')
+            ->add('stockMin')
+            ->add('dateAdded')
+            ->add('supplier')
+            ->add('category')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Product::class,
+        ]);
+    }
+}
